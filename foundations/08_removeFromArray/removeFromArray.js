656e411124aac0778aacd9a121a713e2f98ee22b
mrGarshin <garshin.serg@yandex.ru>
2026-02-28 11:38:27 +0300

    add 08_removeFromArray task solution

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...args) {
+    let result = []
+    for (let i = 0; i < arr.length; i++) {
+        if (args.includes(arr[i]) == false) {
+            result.push(arr[i])
+        }
+    }
+    // взять arr
+    // если arr[i] нет в args, то добавить в result
+    // если arr[i] есть в args, то пропустить
+    // вернуть reuslt
+
+
+    return result
 };
 
 // Do not edit below this line
