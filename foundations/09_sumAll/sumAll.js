d5e573feddd36955d562a57fbe52b19af3aae16b
mrGarshin <garshin.serg@yandex.ru>
2026-03-01 10:28:58 +0300

    add array restructuring in 09_sumAll task solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,15 +1,13 @@
 const sumAll = function(a,b) {
     let result = 0;
-    let min = a;
-    let max = b;
+
     if ((typeof a != typeof 1) || (typeof b != typeof 1) || (a < 0) || (b < 0) || (Number.isInteger(a) != true) || (Number.isInteger(b) != true) ) {
         result = "ERROR"
     } else {
         if (a > b) {
-            min = b;
-            max = a;
+            [a, b] = [b, a]
         }
-        for (let i = min; i <= max; i++) {
+        for (let i = a; i <= b; i++) {
             result += i
         }
     }
