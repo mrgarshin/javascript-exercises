a221704fcc0a46b873b031ef117f53cfaf9f4516
mrGarshin <garshin.serg@yandex.ru>
2026-03-01 10:24:14 +0300

    add 09_summAll task solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let result = 0;
+    let min = a;
+    let max = b;
+    if ((typeof a != typeof 1) || (typeof b != typeof 1) || (a < 0) || (b < 0) || (Number.isInteger(a) != true) || (Number.isInteger(b) != true) ) {
+        result = "ERROR"
+    } else {
+        if (a > b) {
+            min = b;
+            max = a;
+        }
+        for (let i = min; i <= max; i++) {
+            result += i
+        }
+    }
+    return result
 };
 
 // Do not edit below this line
