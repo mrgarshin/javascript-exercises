ddb15d75955dfb55fa0891fdb20e755552f4aec6
mrGarshin <garshin.serg@yandex.ru>
2026-03-02 11:09:14 +0300

    add 12_calculator task solution

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length == 0) {
+    return 0
+  } else {
+    return arr.reduce((acc, elem) => acc + elem)
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.length == 0) {
+    return 0
+  } else {
+    return arr.reduce((acc, elem) => acc * elem)
+  }
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1
+	if ( num == 0) {
+    result = 1
+  } 
+  else {
+    for (let i = 1; i <= num; i++) {
+      result *= i
+    }
+  }
+  return result
 };
 
 // Do not edit below this line
