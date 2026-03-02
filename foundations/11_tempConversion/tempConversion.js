cb2f412d69d67a2ccc950948a38cfc2410ce301a
mrGarshin <garshin.serg@yandex.ru>
2026-03-02 10:35:57 +0300

    add 11_tempConversion task solution

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const result = (temp - 32) * 5/9
+  return +result.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const result = temp * 9/5 + 32
+  return +result.toFixed(1)
 };
 
 // Do not edit below this line
